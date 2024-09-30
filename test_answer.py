d45c299377b2e5dfec7c4b4dbd44f7c870fc0341
Kranti Sri Kalepalli <157083369+kkrantisri@users.noreply.github.com>
2024-09-30 16:11:09 -0400

    Initial commit

--- test_answer.py
+++ test_answer.py
@@ -1,49 +1,129 @@
+import math
+import copy
 import pytest
+
 import answer
 
-def test_number():
-    x, y = answer.number()
-    assert x == 4 ** 5
-    assert y == x / 3
+class TestAnswer():
 
-def test_strings():
-    stevens, stevens_7, length, great, good = answer.strings()
-    assert stevens == "stevens"
-    assert stevens_7 == "stevens" * 7
-    assert length == len(stevens_7)
-    assert great == "stevens is great"
-    assert good == "stevens is good"
+    __correct__ = 0
+    __total__ = 0
 
-def test_list_1D():
-    hoboken, hoboken_list, hoboken_first_item, l, new_l = answer.list_1D()
-    assert hoboken == "hoboken,is,awesome,i,like,it"
-    assert hoboken_list == ["hoboken", "is", "awesome", "i", "like", "it"]
-    assert hoboken_first_item == "hoboken"
-    assert l == sorted([2, 3, 4, 1, 5, 6, 9, 10, 15, 12, 13, -2, -6, 0, 0])
-    assert new_l == sorted([2, 3, 4, 1, 5, 6, 9, 10, 15, 12, 13, -2, -6, 0, 0])[3:10]
+    @classmethod
+    def setup_class(cls):
+        print("Before")
+        cls.__correct__ = 0
+        cls.__total__ = 0
 
-def test_list_2D():
-    A, last_column, a, b = answer.list_2D()
-    assert A == [
-        [1, 4, 5],
-        [6, 10, 11],
-        [12, 17, 38]
-    ]
-    assert last_column == [5, 11, 38]
-    assert a == 38
-    assert b == 6
+    @classmethod
+    def teardown_class(cls):
 
-def test_dictionary():
-    fruit_dict, f = answer.dictionary()
-    assert fruit_dict == {"fruit": "apple", "quantity": 19, "color": "red"}
-    assert f == "apple"
 
-def test_dictionary_nested():
-    Grace, last_name, job = answer.dictionary_nested()
-    assert Grace == {
-        "name": {"first_name": "Grace", "last_name": "Hopper"},
-        "jobs": ["scientist", "engineer", "programmer"],
-        "age": 85
-    }
-    assert last_name == "Hopper"
-    assert job == "programmer"
+        print(f"Score:{(cls.__correct__/cls.__total__)*100}%")
+
+    def test_object_b_equal_to_a(self):
+        TestAnswer.__total__ += 1
+        a = [1,2,[3,4,5]]
+        b, c, d, id_a = answer.objects(a)
+        assert (b == a)
+        TestAnswer.__correct__ += 1
+    
+    def test_obejct_b_id(self):
+        TestAnswer.__total__ += 1
+        a = [1,2,[3,4,5]]
+        b, c, d, id_a = answer.objects(a)
+        assert (id(b) == id(a))
+        TestAnswer.__correct__ += 1
+        
+    def test_obejct_c_value(self):
+        TestAnswer.__total__ += 1
+        a = [1,2,[3,4,5]]
+        b, c, d, id_a = answer.objects(a)
+        assert (c == [1,2,[3,4,5]])
+        TestAnswer.__correct__ += 1
+
+    def test_obejct_c_id(self):
+        TestAnswer.__total__ += 1
+        a = [1,2,[3,4,5]]
+        b, c, d, id_a = answer.objects(a)
+        assert (id(c) != id(a))
+        TestAnswer.__correct__ += 1
+
+    def test_obejct_c_copy(self):
+        TestAnswer.__total__ += 1
+        a = [1,2,[3,4,5]]
+        b, c, d, id_a = answer.objects(a)
+        assert (c == copy.copy(a))
+        TestAnswer.__correct__ += 1
+    
+    def test_obejct_d_id(self):
+        TestAnswer.__total__ += 1
+        a = [1,2,[3,4,5]]
+        b, c, d, id_a = answer.objects(a)
+        assert (id(d) != id(a))
+        TestAnswer.__correct__ += 1
+
+    def test_obejct_d_copy(self):
+        TestAnswer.__total__ += 1
+        a = [1,2,[3,4,5]]
+        b, c, d, id_a = answer.objects(a)
+        assert (d == copy.deepcopy(a))
+        TestAnswer.__correct__ += 1
+    
+    def test_prime_test_1(self):
+        TestAnswer.__total__ += 1
+        number = 10
+        num = answer.prime_number(number)
+        assert(num == [2,3,5,7])
+        TestAnswer.__correct__ += 1
+
+    def test_prime_test_2(self):
+        TestAnswer.__total__ += 1
+        number = 100
+        num = answer.prime_number(number)
+        assert(num == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+        TestAnswer.__correct__ += 1
+    
+    def test_prime_test_3(self):
+        TestAnswer.__total__ += 1
+        number = 3
+        num = answer.prime_number(number)
+        assert(num == [2])
+        TestAnswer.__correct__ += 1
+    
+    def test_while_loop_calculation_1(self):
+        TestAnswer.__total__ += 1
+        output = answer.while_loop("3")
+        assert(output == 6)
+        TestAnswer.__correct__ += 1
+
+    def test_while_loop_calculation_2(self):
+        TestAnswer.__total__ += 1
+        output = answer.while_loop("10")
+        assert(output == 55)
+        TestAnswer.__correct__ += 1
+    
+    def test_while_loop_calculation_3(self):
+        TestAnswer.__total__ += 1
+        output = answer.while_loop("100")
+        assert(output == 5050)
+        TestAnswer.__correct__ += 1
+
+    def test_while_loop_zero(self):
+        TestAnswer.__total__ += 1
+        output = answer.while_loop("0")
+        assert(output == 0)
+        TestAnswer.__correct__ += 1  
+
+    def test_while_loop_not_number(self):
+        TestAnswer.__total__ += 1
+        output = answer.while_loop("Hello")
+        assert(output == "Error")
+        TestAnswer.__correct__ += 1 
+
+    def test_while_loop_decimal_number(self):
+        TestAnswer.__total__ += 1
+        output = answer.while_loop("1.1")
+        assert(output == "Error")
+        TestAnswer.__correct__ += 1
+
