ef8cd858431ad2d4b4bf53c1e536d85dfe6ce833
Kranti Sri Kalepalli <157083369+kkrantisri@users.noreply.github.com>
2024-09-30 16:15:02 -0400

    Initial commit

--- answer.py
+++ answer.py
@@ -1,105 +1,106 @@
+import copy
 
+def objects(a):
+    # The variable a represents a list.
+    # You are given a = [1,2,[3,4,5]]
+    # The variable b also represents a list.
+    # The list b is a copy of list a.
+    # Both list a and list b should have the same id address.
+    
+    # Write the Python statement which will result in list b being a
+    # copy of list a, with list a and list b having the same id address.
+    # ENTER ANSWER BELOW.
+
+    
+
+    # Assign a shallow copy of the list a to the variable c.
+    # (Variable c is a list and is a shallow copy of list a).
+    # ENTER ANSWER BELOW.
+    
+
+
+    # Assign a deep copy of the list a to the variable d.
+    # ENTER ANSWER BELOW.
+
+    
+
+    # Assign the id of list a to the variable id_a
+    # ENTER ANSWER BELOW.
+
+
+    
+    return b, c, d, id_a
+
+
+
+
+# -----------------------------------------------------------------------------------------------------------------
 """
-Python Core object Types
+Write a function prime_number(number). 
+This function will return a list of all the prime numbers
+from 2 up to but not including the variable number.
+Please refer to the sample outputs given below.
+
+Example1:
+        prime_number(10)
+        Output:[2,3,5,7]
+Example2:
+        prime_number(100)
+        Output:[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+Example3:
+        prime_number(7)
+        Output:[2,3,5]
 """
 
-def number():
-    """
-       This is to review numbers and basic operations.
-       """
-    # Write the value 4 to the power of 5 and assign it to variable x.
+#PLEASE COMPLETE THE FOLLOWING PROGRAM.
+
+def prime_number(number):
+    pass #Remove this line and write your program.
+
+
     
-    # Write the value x divided by 3 and assign it to variable y.
     
-    return x,y
-
-def strings():
-    """
-    This is to review numbers and strings and basic operations.
-    """
-
-    # Assign a string "stevens" to a variable stevens.
-
-    # Repeat variable stevens 7 times and assign it to variable stevens_7.
-
-    # Determine the length of stevens_7
-
-    # Concatenate variable stevens with string " is great" and assign it to variable great.
-
-    # Use string slicing to replace "great" with "good" in variable great and assign it to a new variable good.
-
-    return stevens, stevens_7, length, great, good
 
 
-def list_1D():
-    """
-    This is to review basic operations with lists.
-    """
-    s = " hoboken,is,awesome,i,like,it "
-    #Using list slicing, remove the whitespace characters on both side and assign it to a new variable hoboken.
-
-    # Split variable hoboken on a delimiter(comma) into a list of substrings and assign it to a new variable hoboken_list.
-
-    # Get the first item in the hoboken_list and assign it to a new variable hoboken_first_item.
-
-    ####
-    l=[2,3,4,1,5,6,9,10,15,12,13,-2,-6,0,0]
-
-    # Inplace sort list l (use .sort() ).
-
-    # Get the 4th to 10th item in sorted list l and assign them to a new list new_l.
-
-    return hoboken,hoboken_list, hoboken_first_item, l, new_l
-
-def list_2D():
-    # Create a 3 x 3 matrix A as nested list such that
-    #   first row is [1, 4, 5]
-    #   second row is [6, 10, 11]
-    #   third row is [12, 17, 38]
-
-    # Collect the items in the last column of matrix A using list comprehension (with a for loop) and assign it to a new variable last_column.
-
-    # Using indexing, get the item at the last row and last column of A and assign this item to a new variable a.
-
-    # Using indexing, get the item at row 2 and column 1 of A and assign this item to a new variable b.
-
-    return A,last_column, a, b
 
 
-def dictionary():
-    """
-    This is to review basic operations with dictionaries.
-    """
-    # Create a dictionary named fruit_dict that maps:
-    #   "fruit" => "apple"
-    #   "quantity" => 18
-    #   "color" => "red"
+# -----------------------------------------------------------------------------------------------------------------
+"""
+Write a function while_loop(number). The variable
+number is a string.
+
+*If the character in the string is a number, the function
+returns the sum of all integer numbers between 1 and
+number inclusive. Please refer to the sample output
+below:
+Example1:
+        while_loop("3")
+        Output: 6
+        Explanation: 3 + 2 + 1 = 6
+
+*If the character in the string is not a number, (or not an integer
+number) the function returns the string "Error". Please refer to the
+sample output below:
+Example2:
+        while_loop("Hello")
+        Output: "Error"
+        Explanation: Chracters in the string are not numbers.
+        Hint: You may want to consider using a try statement with an except statement.
+
+*If the character in the sting is a number less than one, the function
+returns the number zero as a default. Please refer to the sample
+output below:
+Example3:
+        while_loop("-1")
+        Output: 0
+        Explanation: The number in the string is less than one. The defaul of zero is returned.
+        
+"""
+#PLEASE COMPLETE THE FOLLOWING PROGRAM.
+
+def while_loop(num):
+    total = 0
+    #Write your code here
     
-    # Get the item in dictionary fruit_dict that the key "fruit" maps to. Assign this item to the variable f.
 
-    # Increase the value that key "quantity" maps to by 1.
-
-    return fruit_dict, f
-
-def dictionary_nested():
-    # Create a nested dictionary Grace where:
-    #   "name" => {"first_name" => "Grace", "last_name" => "Hopper"} (a dictionary)
-    #   "jobs" => ["scientist", "engineer"] (a list)
-    #   "age" => 85
-
-    # Get the value of key "last_name" from the subdictionary of key "name" in dictionary Grace. (aka."Hopper")
-
-    # Add "programmer" to the list that key "jobs" maps to.
-
-    # Get the third item in the list that key "job" maps to. (This is the item  you recently added).
-    
-    return Grace,last_name, job
-
-
-
-number()
-strings()
-list_1D()
-list_2D()
-dictionary()
-dictionary_nested()
+    return total
